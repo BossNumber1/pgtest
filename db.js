13871334a74f55f44b50c8cb9407f3917284f514
BossNumber1 <dimaang23@gmail.com>
2022-10-02 22:00:25 +0300

    returned old host

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const Pool = require("pg").Pool
 const pool = new Pool({
   user: "yqwtzxyavhylzh",
   password: "f86a0b527b908055634c2741ec6bac0047c0379bd4efa5703cf00fdf4b1f566a",
-  // host: "ec2-34-231-42-166.compute-1.amazonaws.com",
-  host: "44.193.206.199",
+  host: "ec2-34-231-42-166.compute-1.amazonaws.com",
+  // host: "44.193.206.199",
   port: 5432,
   database: "d5tfeqcnli08u4"
 })
