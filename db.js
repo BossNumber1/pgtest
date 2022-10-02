91c55d19aeac6af1581850363d644f6a767e2d6a
BossNumber1 <dimaang23@gmail.com>
2022-10-02 21:21:47 +0300

    fixed connect settings

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool
 const pool = new Pool({
-  user: "postgres",
-  password: "root",
-  host: "localhost",
+  user: "yqwtzxyavhylzh",
+  password: "f86a0b527b908055634c2741ec6bac0047c0379bd4efa5703cf00fdf4b1f566a",
+  host: "ec2-34-231-42-166.compute-1.amazonaws.com",
   port: 5432,
-  database: "node_postgres"
+  database: "d5tfeqcnli08u4"
 })
 
 module.exports = pool
