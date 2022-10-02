49602f48c354271737d222c9accdfb9e106f16ac
BossNumber1 <dimaang23@gmail.com>
2022-10-02 21:51:15 +0300

    added show error

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,10 +2,14 @@ const db = require('../db')
 
 class UserController {
   async createUser(req, res) {
-    const {name, surname} = req.body
-    const newPerson = await db.query(`INSERT INTO person (name, surname) values ($1, $2) RETURNING *`, [name, surname])
-    
-    res.json(newPerson.rows[0])
+    try {
+      const {name, surname} = req.body;
+      const newPerson = await db.query(`INSERT INTO person (name, surname) values ($1, $2) RETURNING *`, [name, surname]);
+      
+      res.json(newPerson.rows[0]);
+    } catch (e) {
+      res.send("Attention! Your error =", e)
+    }
   }
 
   async getUsers(req, res) {
